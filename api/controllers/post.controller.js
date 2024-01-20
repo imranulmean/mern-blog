f386f27fad05e6bc8934e3ebcc2754d80080cc7c
imranulmean <imranulhasan73@gmail.com>
2024-01-20 06:48:20 +0600

    update

--- api/controllers/post.controller.js
+++ api/controllers/post.controller.js
@@ -1,8 +1,8 @@
 import Post from '../models/post.model.js';
 import { errorHandler } from '../utils/error.js';
 
-export const create = async (req, res, next) => {
-  if (!req.user.isAdmin || !req.user.isMod) {
+export const create = async (req, res, next) => {  
+  if (!req.user.isAdmin && !req.user.isMod) {
     return next(errorHandler(403, 'You are not allowed to create a post'));
   }
   if (!req.body.title || !req.body.content) {
