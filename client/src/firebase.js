c1d3bce329e27f2b2f848241cce8460dc0fa5460
imranulmean <imranulhasan73@gmail.com>
2024-01-20 07:24:03 +0600

    firebase update

--- client/src/firebase.js
+++ client/src/firebase.js
@@ -4,14 +4,25 @@ import { initializeApp } from 'firebase/app';
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: 'mern-blog-b327f.firebaseapp.com',
-  projectId: 'mern-blog-b327f',
-  storageBucket: 'mern-blog-b327f.appspot.com',
-  messagingSenderId: '699397991367',
-  appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
-};
+// const firebaseConfig = {
+//   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+//   authDomain: 'mern-blog-b327f.firebaseapp.com',
+//   projectId: 'mern-blog-b327f',
+//   storageBucket: 'mern-blog-b327f.appspot.com',
+//   messagingSenderId: '699397991367',
+//   appId: '1:699397991367:web:88ff565ef72a182d6b87e2',
+// };
 
+/////////////////////////////////////
+const firebaseConfig = {
+  apiKey: "AIzaSyDyu3zPHWGI7Qmamx9mStYscCPY4hNgMZg",
+  authDomain: "mern-estate-4e89d.firebaseapp.com",
+  projectId: "mern-estate-4e89d",
+  //storageBucket: "mern-estate-4e89d.appspot.com",
+  // storageBucket: 'mern-estate.appspot.com',
+  storageBucket: 'mern-blog-b327f.appspot.com',
+  messagingSenderId: "11007302888",
+  appId: "1:11007302888:web:e3d232a01659511033bd5d"
+};
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
